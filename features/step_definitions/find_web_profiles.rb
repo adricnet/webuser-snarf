5c0dca4eedf7720eb841def68a59303fa9add00e
Adric Net <adric@adric.net>
2013-03-22 14:03:07 -0400

    Inital feature step definitions for finding all the web profiles

---
+++ features/step_definitions/find_web_profiles.rb
@@ -0,0 +1,54 @@
+Given /^a filesystem path (.*)$/ do |fs_path|
+  puts fs_path
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^the program is run$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^output nested zip archives of all users Firefox profiles$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a filesystem path$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^find all usernames$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a user home folder location$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^find all firefox profile folders$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^no users are found$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^return a descriptive error to that effect: "(.*?)"$/ do |error_mesg|
+  puts error_meg
+end
+
+Then /^exit program$/ do
+  exit
+end
+
+Given /^a username (\w+)$/ do |user_name|
+  pending # express the regexp above with the code you wish you had
+  puts "Username: #{user_name}"
+end
+
+Given /^users home folder (\w+) exists$/ do |user_home|
+  pending # express the regexp above with the code you wish you had
+  puts "User home: #{user_home}"
+end
+
+Given /^no Mozilla Firefox folders are found$/ do
+  pending # express the regexp above with the code you wish you had
+end
